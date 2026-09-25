17845bf22185df6b014b7288daf48a762efc3ca5
jvlima1205 <jvlima1205@gmail.com>
2026-09-25 13:57:15 -0300

    Publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  
+  // URL base do seu GitHub Pages
+  site: "https://jvlima1205.github.io",
+
+  // Nome do seu repositório (com uma barra no início)
+  base: "/ManoaBetel.App",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
